1de4ff293bfb6b12dcf2ed39dd83aa1b6cd267d3
bimalsharma1 <bimalsharma1@outlook.com>
2017-03-10 05:59:16 +1000

    a

--- PyNaoWebotsBimal/PyNaoWebotsBimal/Utils/AjustForVCorner.py
+++ PyNaoWebotsBimal/PyNaoWebotsBimal/Utils/AjustForVCorner.py
@@ -10,8 +10,10 @@ from Utils import Helper as h
 
 def AdjustForVCorner(InitialiseNaoRobot): 
     Logger.Log("Adjust to V corner")
+    leftMostAlignmentLimit = 300
+    rightMostAlignmentLimit = 340
 
-    acceptableError = 20
+    acceptableError = 5
 
     adjustedToCorner = False
     rightMostXParallelToLeftMostX = 0
@@ -24,12 +26,21 @@ def AdjustForVCorner(InitialiseNaoRobot):
     diffBtwnLMostParallelXAndBotMostX = 0
     filenameTopCamera = "naoImageTopCamera"
     x=0.5
-    y=0.5
-    turnAngle = 5*almath.TO_RAD
-
+    y=0.4
+    turnAngle = 30*almath.TO_RAD
+    #-ve y walks to the right
+    #+ve turnangle spins to the left
     while not adjustedToCorner:
         im = ip.getImage(InitialiseNaoRobot, "TOP", filenameTopCamera)
         xCentrePostion, yCentrePosition, objectFoundOnBottomCamera, bottomMostPoint,cornerPoints,bl,br,tl,tr = d.DetectColour(filenameTopCamera + ".png", "",im)   
+        
+        #check oin middle point in centre of field of view
+        # if not (bottomMostPoint[0] >= leftMostAlignmentLimit and bottomMostPoint[0] <= rightMostAlignmentLimit):             
+        #     X=2  # was 1.5
+        #     print "bottom y point"
+        #     print bottomMostPoint[1]
+        #     X = ((480.0-bottomMostPoint[1])/float(480.0)) * X
+
         if(cornerPoints[0][1] >= cornerPoints[2][1]):    
             #if leftMostY >= rightMostY
             Logger.Log("left most y > right most y")
@@ -51,18 +62,22 @@ def AdjustForVCorner(InitialiseNaoRobot):
                 return True
             if (diffBtwnBotMostXAndLMostX > diffBtwnRMostParallelXAndBotMostX):
                 #turm left
-                h.WalkToPosition(InitialiseNaoRobot.motionProxy, 0, 0, -(diffBtwnBotMostXAndLMostX-diffBtwnRMostParallelXAndBotMostX)/(diffBtwnBotMostXAndLMostX)*turnAngle) 
+                h.WalkToPositionWaitUntilWalkFinished(InitialiseNaoRobot.motionProxy, 0, 0, -turnAngle) #(diffBtwnBotMostXAndLMostX-diffBtwnRMostParallelXAndBotMostX)/(diffBtwnBotMostXAndLMostX)*
+                print "turning angle left left most y > right most y"
                 time.sleep(4)
-                h.WalkToPosition(InitialiseNaoRobot.motionProxy, 0, y, 0) 
-                Logger.Log("walking dimensions")
+                h.WalkToPositionWaitUntilWalkFinished(InitialiseNaoRobot.motionProxy, 0, -y, 0) 
+                time.sleep(4)
+                Logger.Log("walking dimensions LEFT")
                 Logger.Log(str(y))
                 Logger.Log(str((diffBtwnBotMostXAndLMostX-diffBtwnRMostParallelXAndBotMostX)/(diffBtwnBotMostXAndLMostX)*turnAngle))
             else:
                 #turm right
-                h.WalkToPosition(InitialiseNaoRobot.motionProxy, 0, 0, (diffBtwnRMostParallelXAndBotMostX-diffBtwnBotMostXAndLMostX)/(diffBtwnRMostParallelXAndBotMostX)*turnAngle) 
+                h.WalkToPositionWaitUntilWalkFinished(InitialiseNaoRobot.motionProxy, 0, 0, turnAngle) #(diffBtwnRMostParallelXAndBotMostX-diffBtwnBotMostXAndLMostX)/(diffBtwnRMostParallelXAndBotMostX)*
+                print "turning angle right left most y > right most y"
                 time.sleep(4)
-                h.WalkToPosition(InitialiseNaoRobot.motionProxy, 0, -y, 0) 
-                Logger.Log("walking dimensions")
+                h.WalkToPositionWaitUntilWalkFinished(InitialiseNaoRobot.motionProxy, 0, y, 0) 
+                time.sleep(4)
+                Logger.Log("walking dimensions RIGHT")
                 Logger.Log(str(-y))
                 Logger.Log(str(-(diffBtwnRMostParallelXAndBotMostX-diffBtwnBotMostXAndLMostX)/(diffBtwnRMostParallelXAndBotMostX)*turnAngle))
         else:
@@ -79,17 +94,21 @@ def AdjustForVCorner(InitialiseNaoRobot):
                 return True
             if(diffBtwnLMostParallelXAndBotMostX > diffBtwnBotMostXAndRMostX):
                 #turm left
-                h.WalkToPosition(InitialiseNaoRobot.motionProxy, 0, 0, -(diffBtwnLMostParallelXAndBotMostX-diffBtwnBotMostXAndRMostX)/(diffBtwnLMostParallelXAndBotMostX)*turnAngle) 
+                #-ve y walks to the right
+                #+ve turnangle spins to the left
+                h.WalkToPositionWaitUntilWalkFinished(InitialiseNaoRobot.motionProxy, 0, 0, turnAngle) #(diffBtwnLMostParallelXAndBotMostX-diffBtwnBotMostXAndRMostX)/(diffBtwnLMostParallelXAndBotMostX)*
+                print "turning angle left - right most y > left most y"
                 time.sleep(4)
-                h.WalkToPosition(InitialiseNaoRobot.motionProxy, 0, -y, 0) 
-                Logger.Log("walking dimensions")
+                h.WalkToPositionWaitUntilWalkFinished(InitialiseNaoRobot.motionProxy, 0, -y, 0) 
+                Logger.Log("walking dimensions LEFT")
                 Logger.Log(str(y))
                 Logger.Log(str((diffBtwnLMostParallelXAndBotMostX-diffBtwnBotMostXAndRMostX)/(diffBtwnLMostParallelXAndBotMostX)*turnAngle))
             else:
                 #turm right
-                h.WalkToPosition(InitialiseNaoRobot.motionProxy, 0, 0, (diffBtwnBotMostXAndRMostX-diffBtwnLMostParallelXAndBotMostX)/(diffBtwnBotMostXAndRMostX)*turnAngle) 
+                print "turning angle right - right most y > left most y"
+                h.WalkToPositionWaitUntilWalkFinished(InitialiseNaoRobot.motionProxy, 0, 0, -turnAngle) #(diffBtwnBotMostXAndRMostX-diffBtwnLMostParallelXAndBotMostX)/(diffBtwnBotMostXAndRMostX)*
                 time.sleep(4)
-                h.WalkToPosition(InitialiseNaoRobot.motionProxy, 0, y, 0) 
-                Logger.Log("walking dimensions")
+                h.WalkToPositionWaitUntilWalkFinished(InitialiseNaoRobot.motionProxy, 0, y, 0) 
+                Logger.Log("walking dimensions RIGHT")
                 Logger.Log(str(-y))
                 Logger.Log(str(-(diffBtwnBotMostXAndRMostX-diffBtwnLMostParallelXAndBotMostX)/(diffBtwnBotMostXAndRMostX)*turnAngle))
